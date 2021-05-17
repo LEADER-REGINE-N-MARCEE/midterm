4bdeb6017a11899d9b63fc9a97cf32f969339c9d
projectsyntexia <74484136+projectsyntexia@users.noreply.github.com>
2021-05-17 08:44:27 +0800

    Footer Nav UIUpdate

--- midterm-app/src/components/StockUI.js
+++ midterm-app/src/components/StockUI.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
-export function Label({name}) {
+function formatter({name}) {
     const trimSpaceBackEnd = name.replace(/\s/g,'');
     const firstLetterCapUI = name.charAt(0).toUpperCase() + name.slice(1);
+}
+
+export function Label({name}) {
+    formatter({name});
     return <label for={trimSpaceBackEnd}>{firstLetterCapUI}: </label>
 }
 
 export function Button({name}) {
     return <button>{name}</button>
+}
+
+export function Header({name}) {
+    formatter({name});
+    return <h1 className={name}>{name}</h1>
 }
