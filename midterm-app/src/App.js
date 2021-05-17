6b553cb2bf993a6623439d12e9acbda5df845b7b
Marc Angelo Chiapco <66046436+inn0ichi@users.noreply.github.com>
2021-05-17 08:24:59 +0800

    Update App.js

--- midterm-app/src/App.js
+++ midterm-app/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/home" />
+        </Route>
+        <Route component={Profile} path="/profile" />
+        <Route component={Store} path="/store" />
+        <Route component={Login} path="/login" />
+        <Route component={Register} path="/register" />
+        <Route component={Cart} path="/cart" exact />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
