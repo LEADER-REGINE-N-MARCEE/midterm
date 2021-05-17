c33ed7c014cd7fb70ca722007d97e730b20c050e
cristoferprago <cristoferprago0904@gmail.com>
2021-05-17 09:33:08 +0800

    nav js/css

--- midterm-app/src/components/Nav.js
+++ midterm-app/src/components/Nav.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from 'react'
+import {Link} from 'react-router-dom'
+import './Nav.css'
+import logo from "./logo.png"
 export default function Nav() {
-    return (
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="Home">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="Shop">Shop</Link>
-                    </li>
-                    <li>
-                        <Link to="Buy">Buy</Link>
-                    </li>
-                    <li>
-                        <Link to="Profile">ProfileImage</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    )
+
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/home" >Shop</Link>
+          </li>
+          <li>
+            <Link to="/home" >About Us</Link>
+          </li>
+          
+        </ul>
+
+        <img src={logo} id="logo-nav"/>
+        
+        <Link to="#" id="profile-nav">Profile</Link>
+
+      </nav>
+    </div>
+  )
+
 }
